89b1d401837c5acaffd4930bb26f984eb4cbc5c9
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-06-02 09:08:54 +0900

    2.1.3

---
+++ test213/app/src/main/java/jp/co/stv_tech/test213/MainActivity.kt
@@ -0,0 +1,44 @@
+package jp.co.stv_tech.test213
+
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.floor
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("MainActivity", "onCreate")
+
+        val btClick = findViewById<Button>(R.id.btClick)
+        val listener = HelloListener()
+        btClick.setOnClickListener(listener)
+
+        val iv1 = findViewById<ImageView>(R.id.iv1)
+
+    }
+    private inner class HelloListener : View.OnClickListener {
+        @SuppressLint("Recycle")
+        override fun onClick(v: View?) {
+            val i = Log.i("MainActivity", "onClick")
+
+            val gazouArray = resources.obtainTypedArray(R.array.gazou)
+            val rand = floor(Math.random() * 5).toInt()
+
+            val drawable: Drawable? = gazouArray.getDrawable(rand);
+
+            iv1.setImageDrawable(drawable)
+
+        }
+    }
+}
