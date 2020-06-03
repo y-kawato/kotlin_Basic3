a5e905a0f86cd5c87bb0cb6a032a7491054067b9
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-06-03 16:07:09 +0900

    1.1.7修正

--- test117/app/src/main/java/jp/co/stv_tech/test117/MainActivity.kt
+++ test117/app/src/main/java/jp/co/stv_tech/test117/MainActivity.kt
@@ -16,13 +16,4 @@ class MainActivity : AppCompatActivity() {
 
         fun calculate()
     }
-    class Math : Result {
-
-        override fun calculate() {
-            val a = 2
-            val b = 3
-            println(a + b)
-
-        }
-    }
 }
---
+++ test117/app/src/main/java/jp/co/stv_tech/test117/Math.kt
@@ -0,0 +1,11 @@
+package jp.co.stv_tech.test117
+
+class Math : MainActivity.Result {
+
+    override fun calculate() {
+        val a = 2
+        val b = 3
+        println(a + b)
+
+    }
+}
