a275055d66db3a5e8c7dc966d0f4866e51e9bee9
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-06-04 09:48:38 +0900

    2.1.1修正

--- test211/app/src/main/java/jp/co/stv_tech/test211/MainActivity.kt
+++ test211/app/src/main/java/jp/co/stv_tech/test211/MainActivity.kt
@@ -1,6 +1,5 @@
 package jp.co.stv_tech.test211
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,13 +7,11 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textView: TextView = findViewById(R.id.text)
-
-        textView.text = "初めのAndroidアプリをスタートしました。"
+        textView.setText(R.string.text_name)
     }
 }
