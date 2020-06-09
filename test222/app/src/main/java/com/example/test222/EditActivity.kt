15d895f8b58728b6c86ee9e6e6eb7b7d6f96fb33
川戸裕介 <yu1997010846@gmail.com>
2020-06-09 11:23:46 +0900

    2.2.2

---
+++ test222/app/src/main/java/com/example/test222/EditActivity.kt
@@ -0,0 +1,55 @@
+package com.example.test222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import io.realm.Realm
+import io.realm.kotlin.createObject
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_edit.*
+import java.util.*
+
+class EditActivity : AppCompatActivity() {
+    private val tag = "TodoList"
+    private lateinit var realm:Realm
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+
+        realm = Realm.getDefaultInstance()
+
+        saveBtn.setOnClickListener {
+            var title: String = ""
+            var contents: String = ""
+
+            if (!titleEdit.text.isNullOrEmpty()) {
+                title = titleEdit.text.toString().trim()
+
+
+                if (!contentsEdit.text.isNullOrEmpty()) {
+                    contents = contentsEdit.text.toString()
+                }
+
+                realm.executeTransaction {
+                    val titleId = realm.where<Todo>().max("todoId")
+                    val nextId = (titleId?.toLong() ?: 0L) + 1L
+                    val todo = realm.createObject<Todo>(nextId)
+                    todo.created = Date()
+                    todo.modified = Date()
+                    val cal = Calendar.getInstance()
+                    cal.time = Date()
+                    cal.add(Calendar.DATE, 7)
+                    todo.limitDate = cal.time
+                    todo.title = title
+                    todo.contents = contents
+                }
+                finish()
+            }
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+}
---
+++ test222/app/src/main/java/com/example/test222/TodoRecyclerViewAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.test222
+
+import android.graphics.Insets.add
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.RealmResults
+import android.text.format.DateFormat
+import java.util.*
+import kotlin.concurrent.timer
+
+class TodoRecyclerViewAdapter(realmResults: RealmResults<Todo>) : RecyclerView.Adapter<ViewHolder>() {
+    private val rResults:RealmResults<Todo> = realmResults
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.one_result,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return rResults.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val todo = rResults[position]
+        holder.dateText?.text = DateFormat.format("yyyy/MM/dd kk:mm",todo?.created)
+        holder.limitdateText?.text = DateFormat.format("yyyy/MM/dd kk:mm",todo?.limitDate)
+        holder.titleText?.text = todo?.title.toString()
+        holder.contentsText?.text = todo?.contents.toString()
+
+    }
+}
