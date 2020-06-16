e05a2d5a86c04ca55578e36f291f1dc3f3bd14b3
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-06-16 10:48:16 +0900

    1.1.7修正

--- test117/app/src/main/java/jp/co/stv_tech/test117/MainActivity.kt
+++ test117/app/src/main/java/jp/co/stv_tech/test117/MainActivity.kt
@@ -3,17 +3,15 @@ package jp.co.stv_tech.test117
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),Result {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val result : Result = Math()
-        result.calculate()
+        val math = Math(this)
+        math.calculate()
     }
-    interface Result {
-
-        fun calculate()
+    override fun result(resultValue: Int) {
+        println(resultValue)
     }
 }
--- test117/app/src/main/java/jp/co/stv_tech/test117/Math.kt
+++ test117/app/src/main/java/jp/co/stv_tech/test117/Math.kt
@@ -1,11 +1,14 @@
 package jp.co.stv_tech.test117
 
-class Math : MainActivity.Result {
 
-    override fun calculate() {
-        val a = 2
-        val b = 3
-        println(a + b)
-
+interface Result{
+    fun result(resultValue:Int)
+}
+class Math (private var result:Result) {
+    private val a:Int = 2
+    private val b:Int = 3
+    fun calculate(){
+        val math:Int = a + b
+        result.result(math)
     }
 }
