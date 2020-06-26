5d171057e20ecf9f0b333cdb657110a5c4208d71
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-06-26 10:51:57 +0900

    1.1.5修正

--- test115/app/src/main/java/jp/co/stv_tech/test115/Teacher.kt
+++ test115/app/src/main/java/jp/co/stv_tech/test115/Teacher.kt
@@ -1,8 +1,10 @@
 package jp.co.stv_tech.test115
 
-open class Teacher(){
+open class Teacher(name:String,age:Int,gender:String,salary:Double){
     private var name:String = ""
     private var age:Int = 0
     private var gender:String = ""
     private var salary:Double = 0.0
+
+    private val introduce = "「${name}さんは、${age}歳で給料は${salary*1.3}円です。」"
 }
--- test115/app/src/main/java/jp/co/stv_tech/test115/TenYearsTeacher.kt
+++ test115/app/src/main/java/jp/co/stv_tech/test115/TenYearsTeacher.kt
@@ -1,6 +1,6 @@
 package jp.co.stv_tech.test115
 
-class TenYearsTeacher(name:String,age:Int,gender:String,salary:Double) : Teacher(){
+class TenYearsTeacher(name:String,age:Int,gender:String,salary:Double) : Teacher("山田哲人",23,"男性",200000.0){
     private val introduce = "「${name}さんは、${age}歳で給料は${salary*1.3}円です。」"
 
     fun calculateSalary() {
