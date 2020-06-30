701fa1d8b103b5880abeb06e7b65bf0606823ea7
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-06-30 10:59:33 +0900

    2.4.10

---
+++ test2410/app/src/main/java/jp/co/stv_tech/test2410/MapsActivity.kt
@@ -0,0 +1,32 @@
+package jp.co.stv_tech.test2410
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val stv = LatLng(35.623655, 139.724858)
+        mMap.addMarker(MarkerOptions().position(stv).title("Marker in STV"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(stv))
+    }
+}
