a7abdd7d9439cb2f34ff2868f2c53b3dccf6e062
ysk18_kawato <y.kawato@stv-tech.co.jp>
2020-06-30 16:34:19 +0900

    2.4.11

---
+++ 2411/app/src/main/java/jp/co/stv_tech/a2411/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.co.stv_tech.a2411
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
